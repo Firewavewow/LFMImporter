23b5ec3115fb184868774cef72c4fa32e880c6fc
Fran <fran@servifot.com>
2018-03-23 15:28:04 +0100

    Añadir rutas por defecto

--- src/com/servifot/lfm/lfmimporter/LFMImporter.java
+++ src/com/servifot/lfm/lfmimporter/LFMImporter.java
@@ -1,10 +1,21 @@
 package com.servifot.lfm.lfmimporter;
 
 public class LFMImporter {
+	
+	private static final String USER_HOME = System.getProperty("user.home");
 
+	/** Carpeta de origen de las imágenes */
+	private String m_sourceFolder = "\\\\flashair\\DavWWWRoot";
+	/** Carpeta de destino de las imágenes */
+	private String m_destFolder = USER_HOME + "\\LFMImporter\\flashair\\";
+	
+	/**  */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	
+	
 }
